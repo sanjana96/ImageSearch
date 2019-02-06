8cc901b0de61aa38361d99a43393c89fbea2d04d
sanjana <sanjana0796@gmail.com>
2019-02-06 12:02:38 +0530

    Added test case

---
+++ ImageSearch/__init__.py
@@ -0,0 +1,2 @@
+def find(path, text):
+    return []
---
+++ test/test_image_search.py
@@ -0,0 +1,14 @@
+import ImageSearch
+
+
+def test_image_search():
+    path = 'test/resource'
+
+    result = [{'text': 'Madagascar', 'result': ['text_bottom.jpg']},
+              {'text': 'lazy', 'result': ['simple_text.png']},
+              {'text': 'when you think you have been', 'result': ['meme_text_top_bottim.jpg']}]
+
+    for element in result:
+        assert ImageSearch.find(path, element['text']) == element['result']
+
+# TODO: Test exceptions
