e4c8d9e9188810e7a1b79fad0b2d9d3a641695f0
sanjana <sanjana0796@gmail.com>
2019-02-08 11:24:11 +0530

    Added path processing

--- ImageSearch/__init__.py
+++ ImageSearch/__init__.py
@@ -1,18 +1,26 @@
 import os
-from get_image_text import get_image_text_tesseract
-from search_text import is_text_present
+from get_image_text import get_image_text
+from search_text import search_in_image_text
+
+
+def _is_text_present(path, text):
+    image_text = get_image_text(path)
+    return search_in_image_text(image_text, text)
 
 
 def find(path, text):
 
-    input = os.listdir(path) if os.path.isdir(path) else [os.path.basename(path)]
-
-    dir_name = os.path.dirname(path)
+    if os.path.isdir(path):
+        input = os.listdir(path)
+        dir_name = path
+    else:
+        input = os.path.basename(path)
+        dir_name = os.path.dirname(path)
 
     for file_name in input:
         curr_path = os.path.join(dir_name, file_name)
-        if is_text_present(curr_path, text):
-            yield curr_path
+        if _is_text_present(curr_path, text):
+            yield file_name
 
 
 __all__ = ['find']
