b89792cee8fd366d48dfef06c521d011a461038f
sanjana <sanjana0796@gmail.com>
2019-02-08 11:27:56 +0530

    Added input path processing

--- ImageSearch/get_image_text.py
+++ ImageSearch/get_image_text.py
@@ -6,5 +6,10 @@ except ImportError:
 import pytesseract
 
 
-def get_image_text_tesseract(path):
+def get_image_text(path):
+    # TODO: Add segmentation and more processing here to overcome the limitations of pytesseract
+    return _get_image_text_tesseract(path)
+
+
+def _get_image_text_tesseract(path):
     return pytesseract.image_to_string(Image.open(path))
