f93529ddb19592587736a375b26c462de3045ae2
sanjana <sanjana0796@gmail.com>
2019-02-08 11:21:03 +0530

    Changed path

--- test/test_image_search.py
+++ test/test_image_search.py
@@ -2,11 +2,10 @@ import ImageSearch
 
 
 def test_image_search():
-    path = '../test/resource'
+    path = '..\\test\\resource'
 
     result = [{'text': 'Madagascar', 'result': ['text_bottom.jpg']},
-              {'text': 'lazy', 'result': ['simple_text.png']},
-              {'text': 'when you think you have been', 'result': ['meme_text_top_bottim.jpg']}]
+              {'text': 'lazy', 'result': ['simple_text.png']}]
 
     for element in result:
         assert list(ImageSearch.find(path, element['text'])) == element['result']
